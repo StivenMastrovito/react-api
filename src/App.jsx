d792d5dcb87c8c55e9537bdf278ad66025194915
StivenMastrovito <stivenmastrovito6@gmail.com>
2025-12-03 17:23:01 +0100

    feat: loading che gira

--- src/App.jsx
+++ src/App.jsx
@@ -7,39 +7,50 @@ function App() {
   const [actor, setActor] = useState([]);
   const [mainActor, setMainActor] = useState([])
   const [filterName, setFilterName] = useState("")
+  const [active, setActive] = useState(false)
 
   useEffect(() => {
     axios.get("https://lanciweb.github.io/demo/api/actresses/").then((resp) => {
       const actress = resp.data; // al prossimo render aggiorna actor (actor = [])
-      
+
       axios.get("https://lanciweb.github.io/demo/api/actors/").then((resp) => {
         const actors = resp.data;
-       
+
         setActor([...actress, ...actors])
         setMainActor([...actress, ...actors])
+        setActive(true);
       })
     })
   }, [])
 
-  useEffect(()=>{
+  useEffect(() => {
     const copyArray = actor.filter((item) => item.name.toLowerCase().includes(filterName.toLowerCase()));
     setMainActor(copyArray);
-  },[filterName])
-  
+  }, [filterName])
+
 
   return (
     <>
       <h1>BEST ACTOR 2k25</h1>
-      <div className="filter">
-        <label htmlFor="">Filtra per nome: </label>
-        <input type="text" value={filterName} onChange={(event) => setFilterName(event.target.value)}/>
-      </div>
-      <div className='grid'>
-        {mainActor.map((actor, index) => (
-          <Card key={index} 
-            actor={actor} />
-        ))}
-      </div>
+
+      {active ? (  
+      <>
+        <div className="filter">
+          <label htmlFor="">Filtra per nome: </label>
+          <input type="text" value={filterName} onChange={(event) => setFilterName(event.target.value)} />
+        </div>
+        <div className='grid'>
+          {mainActor.map((actor, index) => (
+            <Card key={index}
+              actor={actor} />
+          ))}
+        </div>
+      </>
+      ) : (
+        <h2 className='loading'>Loading</h2>
+      )
+      }
+
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,12 @@ export default function Card({actor}) {
         <div className={`card ${actor.death_year !== undefined ? "death" : ""}`}>
             <div className="img_card"><img src={actor.image} alt={actor.name} /></div>
             <div className='body_card'>
-                <h3>Nome: {actor.name}</h3>
-                <h3>Nazionalità: {actor.nationality}</h3>
-                <h3>Data di nascita: {actor.birth_year} {actor.death_year !== undefined && `Morte: ${actor.death_year}`}</h3>
-                <h3>Riconoscimenti: {typeof actor.awards === "string" ? actor.awards : actor.awards.join(", ")}</h3>
-                <h3>Film più famosi: {actor.known_for !== undefined ? actor.known_for.join(", ") : actor.most_famous_movies.join(", ") }</h3>
-                <h3>Biografia: {actor.biography}</h3>
+                <p><strong>Nome: </strong>{actor.name}</p>
+                <p><strong>Nazionalità:</strong> {actor.nationality}</p>
+                <p><strong>Data di nascita:</strong> {actor.birth_year} {actor.death_year !== undefined && `Morte: ${actor.death_year}`}</p>
+                <p><strong>Riconoscimenti:</strong> {typeof actor.awards === "string" ? actor.awards : actor.awards.join(", ")}</p>
+                <p><strong>Film più famosi: </strong>{actor.known_for !== undefined ? actor.known_for.join(", ") : actor.most_famous_movies.join(", ") }</p>
+                <p><strong>Biografia:</strong> {actor.biography}</p>
             </div>
 
         </div>
